b92d89fa6a52a7c5ea705fd1bf7eadf3e4b271a9
hedianjie <1163486362@qq.com>
2021-01-26 23:08:26 +0800

    s

--- src/controller/package.ts
+++ src/controller/package.ts
@@ -7,21 +7,48 @@ import path from 'path'
 import fs from 'fs';
 import compressing from 'compressing'
 
+const EXPLAIN = 'explain.json';
+const UPLOAD = '../../uploads';
 
 export default {
+    // 获取文件内容
+    getHashContent(baseUserHashDir) {
+        
 
+        // 返回explain.json中的内容
+        if(fs.existsSync(path.join(baseUserHashDir, EXPLAIN))) {
+            const explain = require(path.join(baseUserHashDir, EXPLAIN));
+
+            // 查看readme是否存在
+            if(explain.readme && fs.existsSync(path.join(baseUserHashDir, explain.readme))) {
+                return {
+                    ...explain,
+                    readme: fs.readFileSync(path.join(baseUserHashDir, explain.readme), 'utf-8')
+                }
+            }
+            else {
+                return {
+                    ...explain
+                }
+            }
+            
+        }
+        else {
+            return {};
+        }
+    },
     /**
      *  生成规则 
-     *      文件目录 filename-hash      路径名(dir)
-     *          文件碎片 hash-index     路径名(fileUrl)
-     *          组合文件 filename-hash  路径名(resultPath)
+     *      文件目录 uploads                baseDir
+     *          用户id  userId              baseUserDir
+     *              文件碎片  {hash}-{num}
+     *              组合文件  {hash}.{mime}
+     *              文件内容  {hash}          baseUserHashDir
      */
     async beginUploadPackage(req:Request, res:Response, next:NextFunction) {
         const bodyParse = verification({
-            total:      {require: true},
+
             hash:       {require: true},
-            filename:   {require: true},
-            mime:       {require: true, type: 'string', scope: ['tar', 'gzip', 'tgz', 'zip']},
 
         }, req.body), body = bodyParse.data;
 
@@ -29,67 +56,47 @@ export default {
             return res.send(format.error([], body));
         }
 
-        let dirList;
-        const userInfo = req.session.userInfo;
-        const userId = userInfo.id;
-        const userDir = path.resolve(__dirname, `../../uploads/${userId}`);
-        const packageDir = path.join(userDir, `${body.filename}-${body.hash}`);
-        const resultPath = path.join(packageDir, `${body.filename}-${body.hash}.${body.mime}`);
+        const _this = this;
+        const userId = req.session.userInfo.id;
+        const hash = body.hash;
+        const baseDir = path.resolve(__dirname, UPLOAD);
+        const baseUserDir = path.join(baseDir, `${userId}`);
+        const baseUserHashDir = path.join(baseUserDir, `${hash}`);
 
-        // 查看组合文件是否存在 如果存在了 就不需要重复上传了 直接返回结果
-        if(fs.existsSync(resultPath)) {
-            // 查看数据库是否已经创建这个包
-            const result = await db.get('package', {
-                hash: body.hash,
-                user_id: userId
-            });
-            // console.log(resultPath)
-            if(result.data.length) {
-                res.send(format.error([], '文件已创建，重复上传！'));
-            }
-            else {
-                const def = {
-                    directory_name: `${body.filename}.${body.mime}`,
-                    hash: body.hash
-                }
-                // 返回explain.json中的内容
-                if(fs.existsSync(path.join(packageDir, 'explain.json'))) {
-                    const explain = require(path.join(packageDir, 'explain.json'));
-    
-                    // 查看readme是否存在
-                    if(explain.readme && fs.existsSync(path.join(packageDir, explain.readme))) {
-    
-    
-                        res.send(format.success({
-                            ...explain,
-                            ...def,
-                            readme: fs.readFileSync(path.join(packageDir, explain.readme), 'utf-8')
-                        }, undefined, 201));
-                    }
-                    else {
-                        res.send(format.success({
-                            ...explain,
-                            ...def,
-                        }, undefined, 201));
-                    }
-                    
-                }
-                else {
-                    res.send(format.success(def, undefined, 201));
-                }
-            }
-            return;
+        /**
+         * 1. 查找数据库中 hash 与 userId 是否存在
+         */
+        const dbResult = await db.get('package', {hash, user_id: userId});
+
+        if(dbResult.data.length) {
+            return res.send(format.error([], '文件已创建，重复上传！'));
         }
 
-        // 查看文件目录是否存在 
-        if(!fs.existsSync(packageDir)) {
-            res.send(format.success([]));
+        /**
+         * 2. 查看/uploads/userId/hash是否存在
+         *  存在：最终生成的文件内容 201
+         */
+        if(fs.existsSync(baseUserHashDir)) {
+            return res.send(format.success({hash, ..._this.getHashContent(baseUserHashDir)}, '文件传输完成，未创建', 201));
+        }
+
+        /**
+         * 3. 查看/uploads/userId/中所有碎片包({hash}-{num})
+         *  存在：返回所有碎片包名(取名{num})集合
+         *  不粗在：返回空数组
+         */
+        if(fs.existsSync(baseUserDir)) {
+            const chunkList = fs.readdirSync(baseUserDir)
+                                .filter(item => new RegExp(`^${hash}-[0-9]+$`).test(item))
+                                .map(item => item.split('-')[1]);
+            
+            return res.send(format.success(chunkList));
         }
         else {
-            dirList = fs.readdirSync(packageDir);
-            dirList = dirList.map(item => item.split('-')[1]);
-            res.send(format.success(dirList));
+            return res.send(format.success([]));
+            
         }
+
     },
 
     /**
@@ -102,7 +109,7 @@ export default {
         const bodyParse = verification({
             
             total:      {require: true},
-            filename:   {require: true},
+            // filename:   {require: true},
             hash:       {require: true},
             mime:       {require: true, type: 'string', scope: ['tar', 'gzip', 'tgz', 'zip']},
 
@@ -112,194 +119,181 @@ export default {
             return res.send(format.error(body));
         }
 
-        let dirList;
-        const userInfo = req.session.userInfo;
-        const userId = userInfo.id;
-        const userDir = path.resolve(__dirname, `../../uploads/${userId}`);
-        const packageDir = path.join(userDir, `${body.filename}-${body.hash}`);
-        const resultPath = path.join(packageDir, `${body.filename}-${body.hash}.${body.mime}`);
+        const _this = this;
+        const userId = req.session.userInfo.id;
+        const hash = body.hash;
+        const baseDir = path.resolve(__dirname, UPLOAD);
+        const baseUserDir = path.join(baseDir, `${userId}`);
+        const baseUserHashDir = path.join(baseUserDir, `${hash}`);
 
-        // 查看是否有用户目录
-        if(!fs.existsSync(userDir)) {
-            return res.send(format.error(body.filename, '未找到用户文件夹，请重新上传'));
+        const packageUrl = path.join(baseUserDir, `${hash}.${body.mime}`);
+
+        /**
+         * 1. 查找/uploads/userId文件夹是否存在
+         */
+        if(!fs.existsSync(baseUserDir)) {
+            return res.send(format.error([], '未找到用户文件夹，请重新上传'));
         }
 
-        // 查看是否有存放目录
-        if(fs.existsSync(packageDir)) {
-            dirList = fs.readdirSync(packageDir);
-        }
-        else {
-            return res.send(format.error(body.filename, '未找到包存放文件夹，请重新上传'));
+        const chunkList = fs.readdirSync(baseUserDir)
+                            .filter(item => new RegExp(`^${hash}-[0-9]+$`).test(item))
+                            .sort((a, b) => a.split('-')[1] - b.split('-')[1])
+                            .map(item => path.join(baseUserDir, item));
+        
+        /**
+         * 2. 验证/uploads/userId/{hash}-{num}的数量与total是否一致
+         */
+        if(chunkList.length != body.total) {
+            return res.send(format.error({expect: body.total, reality: chunkList.length}, '包数量验证错误，请重新上传'));
         }
 
-        // 验证文件数量是否符合
-        if(dirList.length != body.total) {
-            return res.send(format.error({expect: body.total, reality: dirList.length}, '包数量验证错误，请重新上传'));
-        }
+        /**
+         * 3. 生成名为{hash}.{mime}的文件
+         */
+        concat(chunkList, packageUrl, err => {
 
-        // 文件排序
-        dirList = dirList.sort((a, b) =>{
-            let aIndex = a.split('-')[1];
-            let bIndex = b.split('-')[1];
-            return aIndex-bIndex;
-        }).map(item => path.join(packageDir, item))
-  
-        concat(dirList, resultPath, (err) => {
+            // 删除所有碎片
+            chunkList.forEach(url => fs.unlinkSync(url));
+
             if(err) {
-                res.send(format.error([], err));
+                return res.send(format.error([], '文件合成失败，文件已损坏，请重新上传'));
             }
-            else {
-                // 删除原文件碎片
-                dirList.forEach(url => fs.unlinkSync(url));
-                // 解压压缩包
-                compressing[body.mime].uncompress(resultPath, packageDir)
-                .then(() => {
 
-                    // 查看解压后的文件 如果是文件夹 则把文件移出来
-                    let nextDirList = fs.readdirSync(packageDir);
-                    // 排除{原}压缩文件
-                    nextDirList.splice(nextDirList.indexOf(`${body.filename}-${body.hash}.${body.mime}`), 1);
-                    if(nextDirList.length === 1) {
-                        // 解压后文件的地址
-                        const furl = path.join(packageDir, nextDirList[0]);
-                        const stat = fs.statSync(furl);
-                        if(stat.isDirectory()) {
-                            const flist = fs.readdirSync(furl);
-                            // 循环移除零件
-                            flist.forEach(item => fs.renameSync(
-                                path.join(furl, item),
-                                path.join(packageDir, item)
-                            ));
+            /**
+             * 4. 创建名为{hash}的文件夹
+             */
+            fs.mkdirSync(baseUserHashDir);
 
+            /**
+             * 5. 解压生成的包存放到{hash}文件夹中
+             */
+            compressing[body.mime].uncompress(packageUrl, baseUserHashDir)
+            .then(() => {
+
+                /**
+                 * 6. 查看解压出来的文件是否为单一文件*夹
+                 */
+                const a = fs.readdirSync(baseUserHashDir);
+                if(a.length === 1) {
+                    const p = path.join(baseUserHashDir, a[0])
+                    if(fs.statSync(p).isDirectory()) {
+                        // 提取单一文件夹中的文件
+                        const f = fs.readdirSync(p);
+                        // 循环移动零件
+                        f.forEach(item => fs.renameSync(
+                            path.join(p, item),
+                            path.join(baseUserHashDir, item)
+                        ));
+
+                        setTimeout(() => {
                             try {
-                                fs.unlinkSync(furl);
+                                fs.unlinkSync(p);
                             }
                             catch(e) {}
-                        }
+                        })
                     }
+                }
 
-                    const def = {
-                        directory_name: `${body.filename}.${body.mime}`,
-                        hash: body.hash
-                    }
-                    // 返回explain.json中的内容
-                    if(fs.existsSync(path.join(packageDir, 'explain.json'))) {
-                        const explain = require(path.join(packageDir, 'explain.json'));
+                /**
+                 * 7. 返回读取后的文件
+                 */
+                return res.send(format.success({hash, ..._this.getHashContent(baseUserHashDir)}));
 
-                        // 查看readme是否存在
-                        if(explain.readme && fs.existsSync(path.join(packageDir, explain.readme))) {
-
-
-                            res.send(format.success({
-                                ...explain,
-                                ...def,
-                                readme: fs.readFileSync(path.join(packageDir, explain.readme), 'utf-8')
-                            }));
-                        }
-                        else {
-                            res.send(format.success({
-                                ...explain,
-                                ...def,
-                            }));
-                        }
-                        
-                    }
-                    else {
-                        res.send(format.success(def));
-                    }
-                    
-                })
-                .catch(err =>{
-                    res.send(format.error(resultPath, err));
-                })
-            }
-        });
+            })
+            .catch(err => {
+                fs.unlinkSync(packageUrl);
+                return res.send(format.error([], '解压上传包失败，请重新上传！'));
+            })
+        })
         
     },
 
     /**
-     *  生成规则 
-     *      文件目录 filename-hash      路径名(dir)
-     *          文件碎片 hash-index     路径名(fileUrl)
-     *          组合文件 filename-hash  路径名(resultPath)
+     *  上传
+    /**
+     *
+     *
+     * @param {Request} req
+     * @param {Response} res
+     * @param {NextFunction} next
+     * @returns
      */
     async uploadPackage(req:Request, res:Response, next:NextFunction) {
         const bodyParse = verification({
 
             file:       {require: true},
-            filename:   {require: true},
             hash:       {require: true},
             index:      {require: true},
-            mime:       {require: true, type: 'string', scope: ['tar', 'gzip', 'tgz', 'zip']}
 
         }, req.body), body = bodyParse.data;
 
-        if(!bodyParse.success) {
+        const unlinkFile = () => {
             for(let k in req.body) {
                 if(req.body[k] instanceof File) {
                     fs.unlinkSync(bodyParse.source[k].path);
                 }
             }
+        }
+
+        /**
+         * 与其他验证不同，如果是上传文件验证失败后，需要把上传上来的文件同时删除掉
+         */
+        if(!bodyParse.success) {
+            unlinkFile();
             return res.send(format.error(body.index, body));
         }
         
-        const userInfo = req.session.userInfo;
-        const userId = userInfo.id;
-        const userDir = path.resolve(__dirname, `../../uploads/${userId}`);
-        const packageDir = path.join(userDir, `${body.filename}-${body.hash}`);
-        const resultPath = path.join(packageDir, `${body.filename}-${body.hash}.${body.mime}`);
-        const fileUrl = path.join(packageDir, `${body.hash}-${body.index}`);
+        const _this = this;
+        const userId = req.session.userInfo.id;
+        const hash = body.hash;
+        const baseDir = path.resolve(__dirname, UPLOAD);
+        const baseUserDir = path.join(baseDir, `${userId}`);
+        const baseUserHashDir = path.join(baseUserDir, `${hash}`);
 
-        // 查看是否有用户目录
-        if(!fs.existsSync(userDir)) {
-            try {
-                fs.mkdirSync(userDir);
-            }
-            catch(e) {
-                return res.send(format.error(body.index, `创建用户文件夹失败，文件夹名称 ${body.filename} !`))
-            }
-        }
+        const chunkUrl = path.join(baseUserDir, `${body.hash}-${body.index}`);
         
-        // 查看是否有存放目录
-        if(!fs.existsSync(packageDir)) {
+        /**
+         * 1. 查看整体文件包是否已经生成 '../../uploads/userId/hash'
+         */
+        if(fs.existsSync(baseUserHashDir)) {
+            unlinkFile();
+            return res.send(format.error([], '文件已生成，无需重复上传！'));
+        }
+
+        /**
+         * 2. 查看文件碎片是否上传过 '../../uploads/userId/hash-{num}'
+         */
+        if(fs.existsSync(chunkUrl)) {
+            unlinkFile();
+            return res.send(format.success({index: body.index}, '上传成功!'));
+        }
+
+        /**
+         * 3. 查看../../uploads/userId文件夹是否存在，没存在则创建
+         */
+        if(!fs.existsSync(baseUserDir)) {
             try {
-                fs.mkdirSync(packageDir);
+                fs.mkdirSync(baseUserDir);
             }
             catch(e) {
-                return res.send(format.error(body.index, `创建包文件夹失败，文件夹名称 ${body.filename} !`))
+                unlinkFile();
+                return res.send(format.error(body.index, `创建用户文件夹失败 !`))
             }
         }
-       
-        // 查看最后文件是否已经生成 如果已经生成了 则不操作直接返回成功
-        if(fs.existsSync(resultPath)) {
-            res.send(format.error([], '文件已生成，无需重复上传！'));
-            printLog('big upload', '文件已生成，无 需重复上传！');
-            return;
+
+        /**
+         * 4. 把默认文件移动到../../uploads/userId文件夹下
+         */
+
+        try {
+            fs.renameSync(body.file.path, baseUserDir);
+            return res.send(format.success({index: body.index}, '上传成功!'));
         }
-    
-        // 查看文件包是否存在 存在则删除内存中的包 直接返回成功
-        if(fs.existsSync(fileUrl)) {
-            fs.unlinkSync(body.file.path);
-            res.send(format.success({
-                index: body.index,
-                url: fileUrl
-            }, '上传成功!'));
-            printLog('big upload', '文件已存在')
+        catch(e) {
+            unlinkFile();
+            return res.send(format.error(body.index, `移动碎片出错，碎片索引 ${body.index} !`))
         }
-        // 不存在则把文件移动过来
-        else {
-            try {
-                fs.renameSync(body.file.path, fileUrl);
-                res.send(format.success({
-                    index: body.index,
-                    url: fileUrl
-                }, '上传成功!'));
-                printLog('big upload', '新上传的文件包')
-            }
-            catch(e) {
-                res.send(format.error(body.index, `移动包出错，包索引 ${body.index} !`))
-            }
-        }
+
     },
 
 
